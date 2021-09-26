4ff071720b7710d8c398f3a688916edd550b11da
MadCrispo <maxcrispino@gmail.com>
2021-09-26 13:48:30 +0200

    Cinemachine e inventario semplice

---
+++ Assets/Collectible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public Item m_self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collide");
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            InventoryManager.Instance.ModifyInventory(m_self, true);
+            Destroy(gameObject);
+        }
+    }
+
+   
+}
---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public ItemLogic[] ItemSprites;
+    public Dictionary<Item, GameObject> Inventory=new Dictionary<Item, GameObject>();
+    public GameObject ItemUI,Bag;
+    private void Awake()
+    {
+        if (Instance == null|| Instance!=this)
+            Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (ItemLogic i in ItemSprites)
+        {
+            if (i.alreadyimage)
+            {
+                GameObject newitem = Instantiate<GameObject>(ItemUI, Bag.transform);
+                newitem.GetComponentInChildren<Image>().sprite = ItemSprites[(int)i.item].sprite;
+                newitem.GetComponentInChildren<Text>().text = "0";
+
+                Inventory.Add(i.item, newitem);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public int HowMany(Item item)
+    {
+        if (Inventory.ContainsKey(item))
+        {
+            return int.Parse(Inventory[item].GetComponentInChildren<Text>().text);
+        }
+        return 0;
+    }
+    public void ModifyInventory(Item item,bool adding)
+    {
+        if (adding)
+        {
+            if (Inventory.ContainsKey(item))
+            {
+                int value = int.Parse(Inventory[item].GetComponentInChildren<Text>().text);
+                value++;
+                Inventory[item].GetComponentInChildren<Text>().text = ""+value;
+            }
+            else
+            {
+                GameObject newitem = Instantiate<GameObject>(ItemUI,Bag.transform);
+                newitem.GetComponentInChildren<Image>().sprite = ItemSprites[(int)item].sprite;
+                newitem.GetComponentInChildren<Text>().text = "1";
+                
+                Inventory.Add(item,newitem);
+            }
+        }
+        else
+        {
+            if (Inventory.ContainsKey(item))
+            {
+                int value = int.Parse(Inventory[item].GetComponentInChildren<Text>().text);
+                value--;
+                if (value == 0)
+                {
+                    Destroy(Inventory[item]);
+                    Inventory.Remove(item);
+                }
+                else
+                {
+                    value = int.Parse(Inventory[item].GetComponentInChildren<Text>().text);
+                    value--;
+                    Inventory[item].GetComponentInChildren<Text>().text = "" + value;
+                }
+            }
+        }
+    }
+   
+    
+}
+public enum Item
+{
+    Gem=0,key=1
+}
+[System.Serializable]
+public class ItemLogic
+{
+    public Sprite sprite;
+    public Item item;
+    public bool alreadyimage=false;
+}
