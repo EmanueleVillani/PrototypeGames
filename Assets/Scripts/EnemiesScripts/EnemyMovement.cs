676514fc4a61d34d0f77c84eb3cf8c1c0a36ac7d
Zodiark92 <91218477+Zodiark92@users.noreply.github.com>
2021-09-28 22:43:55 +0200

    Enemies scripts aggiunti

---
+++ Assets/Scripts/EnemiesScripts/EnemyMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5;
+
+    private Animator anim;
+
+    private SpriteRenderer sr;
+
+    [SerializeField]
+    private float maxDistanceEnemyPlayer = 10f;  //When the distance Enemy-Player is less than this variable the enemy will moves towards the player.
+
+    [SerializeField]
+    private float minDistanceEnemyPlayer = 0.2f; //When the Enemy is close to the player will starts to attack.
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player");
+
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    private void Start()
+    {
+        anim.Play("Idle");
+
+    }
+
+
+    private void Update()
+    {
+        if ((Vector2.Distance(transform.position, player.transform.position) < maxDistanceEnemyPlayer))
+        {
+            if((Vector2.Distance(transform.position, player.transform.position) > minDistanceEnemyPlayer))
+            {
+               
+                float step = moveSpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+
+                anim.SetBool("Walk",true);
+
+            }
+            else
+            {
+                AttackPlayer();
+                anim.SetBool("Walk", false);
+
+            }
+
+        }
+
+        if (player.gameObject.transform.position.x < transform.position.x)
+            sr.flipX = false;
+        else
+            sr.flipX = true;
+
+        if (gameObject.CompareTag("Ghost"))
+        {
+            if (player.gameObject.transform.position.x < transform.position.x)
+                sr.flipX = true;
+            else
+                sr.flipX = false;
+
+        }
+   
+    }
+
+
+    void AttackPlayer()
+    {
+
+       
+
+          
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/EnemiesScripts/TriggerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ghost;
+
+    [SerializeField]
+    private float distanceSpawn = 5f;
+
+
+
+    private bool hasSpawned;  //this bool controls the spawn of the ghost
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasSpawned)   
+        {
+            if (collision.CompareTag("Player"))
+            {
+
+                if (Random.Range(0, 2) == 1)
+                    Instantiate(ghost, new Vector3(transform.position.x - distanceSpawn, transform.position.y, 0), Quaternion.identity);
+                else
+                    Instantiate(ghost, new Vector3(transform.position.x + distanceSpawn, transform.position.y, 0), Quaternion.identity);
+
+                hasSpawned = true;
+            }
+        }
+        
+    }
+}
