853c320dbf324f8239b0d33662d323c9ef65420c
Emanuele Villani <manu.lebon@hotmail.it>
2021-10-01 21:29:28 +0200

    ok
    ok

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Func distruzione gemma 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+           // Destroy(gameObject);
+        }
+    }
+}
