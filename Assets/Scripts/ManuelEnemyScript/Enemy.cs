14fd58a1b47d79f9feed6069af6833cdb976039f
Emanuele Villani <manu.lebon@hotmail.it>
2021-10-06 20:58:55 +0200

    ZombieEnemy
    Aggiunto Zombie fatto animazione e una parte di script

---
+++ Assets/Scripts/ManuelEnemyScript/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+    protected Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(-1, rb.velocity.y);
+    }
+}
