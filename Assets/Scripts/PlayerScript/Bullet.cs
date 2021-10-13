9b1687a2bb8222d1b95b09a58a6d0d9faefb89db
Zodiark92 <91218477+Zodiark92@users.noreply.github.com>
2021-10-13 18:03:04 +0200

    Aggiunti sprites 2D e modelli zombie
    I modelli zombie sono nella cartella ZombieModels (in SPRITES), Prefabs. Sono già completi di animazione.

---
+++ Assets/Scripts/PlayerScript/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speedBullet=5;
+
+    private void Update()
+    {
+       
+        transform.Translate(speedBullet * Time.deltaTime * Vector3.right);
+        
+    }
+
+
+
+
+
+
+}
